9898569fd2e388f3ce420236a1615b5dd981aa59
chenjiahau <chenjiahau@gmail.com>
2023-11-24 10:01:54 +0800

    Django Rest Framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def hello(request):
     data = {
         "message": "Welcome to the API!",
     }
 
-    return JsonResponse(data)
+    return Response(data)
