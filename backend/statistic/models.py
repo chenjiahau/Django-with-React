a43b4d4fe3719991bb570a595867a5f87fc54fc2
ivan <ivan.chen@dlinkcorp.com>
2023-11-26 16:13:33 +0800

    1. BigLottyer's wrong number validators 2. Add the least two data into

--- backend/statistic/models.py
+++ backend/statistic/models.py
@@ -18,37 +18,37 @@ class BigLottery(models.Model):
     number1 = models.IntegerField(
         verbose_name='第一個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number2 = models.IntegerField(
         verbose_name='第二個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number2 = models.IntegerField(
         verbose_name='第二個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number3 = models.IntegerField(
         verbose_name='第三個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number4 = models.IntegerField(
         verbose_name='第四個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number5 = models.IntegerField(
         verbose_name='第五個號碼',
         help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        validators=[MinValueValidator(1), MaxValueValidator(49)]
     )
     number6 = models.IntegerField(
-        verbose_name='第六個號碼',
-        help_text='請輸入1~49的整數',
-        validators=[MinValueValidator(1), MaxValueValidator(9)]
+        verbose_name = '第六個號碼',
+        help_text = '請輸入1~49的整數',
+        validators = [MinValueValidator(1), MaxValueValidator(49)]
     )
     created_at = models.DateTimeField(
         auto_now_add=True
@@ -56,3 +56,10 @@ class BigLottery(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True
     )
+
+    def __str__(self):
+        return f'{self.period}'
+
+    class Meta:
+        ordering=['-period']
+        verbose_name_plural='BigLotteries'
